bfaf2ed7917e8d1a7ff9ad5320104e5efe681c1b
gstv <gustavdlim@gmail.com>
2022-10-14 18:10:43 -0300

    change degreeToradians.js name to utils.js
    add new funcction to utils.js

--- degreeToRadians.js
+++
@@ -1,7 +0,0 @@
-// Cast degree to radian needed to work with math.sin and math.cos functions
-
-// @param {Number}
-function degreeToRadians(degree) {
-	let pi = Math.pi;
-	return degree * pi / 180;
-}
---
+++ utils.js
@@ -0,0 +1,22 @@
+/** Cast degree to radian needed to work with math.sin and math.cos functions
+ * @param {Number}
+*/
+function degreeToRadians(degree) {
+	let pi = Math.pi;
+	return degree * pi / 180;
+}
+
+/**
+ * @param {Number} x1
+ * @param {Number} y1
+ * @param {Number} x2
+ * @param {Number} y2
+ * @param {String} cssColor
+*/
+function drawLine(x1, y1, x2, y2, cssColor) {
+	screenContext.strokeStyle = cssColor;
+	screenContext.beginPath();
+	screenContext.moveTo(x1, y1);
+	screenContext.lineTo(x2. y2);
+	screenContext.stroke();
+}
