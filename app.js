0ecac48f0805cd0f815727a1b4c965f163c6aa53
gstv <gustavdlim@gmail.com>
2022-10-14 19:38:33 -0300

    add raycasting logic and the program now have floor, wall and ceiling

--- app.js
+++ app.js
@@ -1,56 +1,72 @@
 let data = {
-	screen: {
-		width: 640,
-		height: 480,
-		halfWidth: null,
-		halfHeight: null
-	},
-	render: {
-		delay: 30
-	},
-	rayCasting: {
-		incrementAngle: null,
-		precision: 64
-	},
-	player: {
-		fov: 60,
-		halfFov: null,
-		x: 2,
-		y: 2,
-		angle: 90
-	},
-	map: [
-        [1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,1],
-        [1,0,0,1,1,0,1,0,0,1],
-        [1,0,0,1,0,0,1,0,0,1],
-        [1,0,0,1,0,0,1,0,0,1],
-        [1,0,0,1,0,1,1,0,0,1],
-        [1,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,1],
-        [1,1,1,1,1,1,1,1,1,1],
-    ]
-}
+  screen: {
+    width: 1024,
+    height: 720,
+    halfWidth: null,
+    halfHeight: null,
+  },
+  render: {
+    delay: 30,
+  },
+  rayCasting: {
+    incrementAngle: null,
+    precision: 64,
+  },
+  player: {
+    fov: 60,
+    halfFov: null,
+    x: 2,
+    y: 2,
+    angle: 90,
+    speed: {
+      movement: 0.3,
+      rotation: 5.0,
+    },
+  },
+  map: [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ],
+  key: {
+    up: "KeyW",
+    down: "KeyS",
+    left: "KeyA",
+    right: "KeyD",
+  },
+};
 
 // Calculated data
-data.screen.halfHeight = data.screen.height / 2;
 data.screen.halfWidth = data.screen.width / 2;
+data.screen.halfHeight = data.screen.height / 2;
 // incrementAngle = The value to increment for each ray in relation of the screen width
 data.rayCasting.incrementAngle = data.player.fov / data.screen.width;
 data.player.halfFov = data.player.fov / 2;
 
-
 // Canvas
-const screen = document.createElement('canvas');
+const screen = document.createElement("canvas");
 screen.width = data.screen.width;
 screen.height = data.screen.height;
-screen.style.border = '1px solid black';
+screen.style.border = "1px solid black";
 // The appendChild() method appends a node (element) as the last child of an element.
 document.body.appendChild(screen);
 
-function clearScreen(x, y, width, height) {
-	screenContext.clearRect(0, 0, width, height);
+// Canvas context
+const screenContext = screen.getContext("2d");
+
+/** Cast degree to radian needed to work with math.sin and math.cos functions
+ * @param {Number}
+ */
+function degreeToRadians(degree) {
+  let pi = Math.PI;
+  return (degree * pi) / 180;
 }
 
 /**
@@ -59,30 +75,88 @@ function clearScreen(x, y, width, height) {
  * @param {Number} x2
  * @param {Number} y2
  * @param {String} cssColor
-*/
+ */
 function drawLine(x1, y1, x2, y2, cssColor) {
-	screenContext.strokeStyle = cssColor;
-	screenContext.beginPath();
-	screenContext.moveTo(x1, y1);
-	screenContext.lineTo(x2. y2);
-	screenContext.stroke();
+  screenContext.strokeStyle = cssColor;
+  screenContext.beginPath();
+  screenContext.moveTo(x1, y1);
+  screenContext.lineTo(x2, y2);
+  screenContext.stroke();
 }
 
-/** Cast degree to radian needed to work with math.sin and math.cos functions
- * @param {Number}
-*/
-function degreeToRadians(degree) {
-	let pi = Math.pi;
-	return degree * pi / 180;
+//Start
+main();
+
+// Main Loop
+function main() {
+  setInterval(function () {
+    clearScreen();
+    rayCasting();
+  }, data.render.delay);
 }
 
 // Raycasting logic
 function rayCasting() {
+  let rayAngle = data.player.angle - data.player.halfFov;
+  for (let rayCount = 0; rayCount < data.screen.width; rayCount++) {
+    // Ray data
+    let ray = {
+      x: data.player.x,
+      y: data.player.y,
+    };
 
+    // Ray path incrementers
+    let rayCos =
+      Math.cos(degreeToRadians(rayAngle)) / data.rayCasting.precision;
+    let raySin =
+      Math.sin(degreeToRadians(rayAngle)) / data.rayCasting.precision;
+
+    // Wall finder
+    let wall = 0;
+    while (wall == 0) {
+      ray.x += rayCos;
+      ray.y += raySin;
+      wall = data.map[Math.floor(ray.y)][Math.floor(ray.x)];
+    }
+
+    // Pythagoras theorem
+    let distance = Math.sqrt(
+      Math.pow(data.player.x - ray.x, 2) + Math.pow(data.player.y - ray.y, 2)
+    );
+
+    // Wall height
+    let wallHeight = Math.floor(data.screen.halfHeight / distance);
+
+    // Draw
+    drawLine(
+      rayCount,
+      0,
+      rayCount,
+      data.screen.halfHeight - wallHeight,
+      "cyan"
+    );
+    drawLine(
+      rayCount,
+      data.screen.halfHeight - wallHeight,
+      rayCount,
+      data.screen.halfHeight + wallHeight,
+      "red"
+    );
+    drawLine(
+      rayCount,
+      data.screen.halfHeight + wallHeight,
+      rayCount,
+      data.screen.height,
+      "green"
+    );
+
+    // Increment;
+    rayAngle += data.rayCasting.incrementAngle;
+  }
 }
-
-function main() {
-	setInterval(function() {
-		clearScreen();
-	})
+/**
+ * Clear screen
+ */
+function clearScreen() {
+  screenContext.clearRect(0, 0, data.screen.width, data.screen.height);
 }
